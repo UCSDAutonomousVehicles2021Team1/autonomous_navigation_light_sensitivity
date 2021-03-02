7a1f5101d3d505ea2a86eb2943aa0e3068ba92e6
Youngseo Do <ysdo364@gmail.com>
2021-03-01 16:56:09 -0800

    Update run.py

--- run.py
+++ run.py
@@ -15,3 +15,33 @@ def main(targets):
     evaluate_config = json.load(open('config/evaluate-params.json'))
     inference_config = json.load(open('config/inference-params.json'))
     test_config = json.load(open('config/test-params.json'))
+
+    if 'data' in targets:
+        move_data(**data_config)
+        
+    
+    if 'eda' in targets:
+        main_eda(**eda_config)
+        
+    if 'comparisons' in targets:
+        
+        
+    
+    if 'evaluate' in targets:
+        best_model_name = find_best_model(model_names, **evaluate_config)
+        
+    
+    if 'all' in targets:
+        move_data(**data_config)
+        main_eda(**eda_config)
+        main_eda(**eda_config)
+        convert_notebook(**eda_config)
+        model_names = train_models(**training_config)
+        best_model_name = find_best_model(model_names, **evaluate_config)
+        print("Found best model: {}".format(best_model_name))
+        
+    
+if __name__ == '__main__':
+    
+    targets = sys.argv[1:]
+    main(targets)
