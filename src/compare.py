bcf9604d5b87f58728720e5d2b49c7f845129d32
Youngseo Do <ysdo364@gmail.com>
2021-03-04 13:12:13 -0800

    Update compare.py

--- src/compare.py
+++ src/compare.py
@@ -1,12 +1,16 @@
 # import the necessary packages
 import os
-from PIL import Image
 from skimage.metrics import structural_similarity as ssim
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-import pandas as pd
 
+
+def view_results(files, outdir):
+    os.makedirs(outdir, exist_ok = True)
+    baseline_results(files[0], files[1], outdir)
+    tuned_results(files[0], files[2], outdir)
+    
 
 def calculate_mse(imageA, imageB):
     # the 'Mean Squared Error' between the two images is the
@@ -19,8 +23,7 @@ def calculate_mse(imageA, imageB):
     return error
  
 
-def compare_images(imageA, imageB, outdir):
-    os.makedirs(outdir, exist_ok = True)
+def baseline_results(imageA, imageB, outdir):
     # compute the mean squared error and structural similarity
     # index for the images
     s = ssim(imageA, imageB, multichannel=True)
@@ -36,9 +39,30 @@ def compare_images(imageA, imageB, outdir):
     ax = fig.add_subplot(1, 2, 2)
     plt.imshow(imageB, cmap = plt.cm.gray)
     plt.axis("off")
-    # show the images
-    plt.show()
     # save figure
-    plt.savefig(os.path.join(outdir, 'metrics.jpg')
+    plt.savefig(os.path.join(outdir, 'baseline.jpg'))
+    plt.close()
     
-    return 
+    return
+    
+def tuned_results(imageA, imageC, outdir):
+    # compute the mean squared error and structural similarity
+    # index for the images
+    s = ssim(imageA, imageC, multichannel=True)
+    m = calculate_mse(imageA, imageC)
+    # setup the figure
+    fig = plt.figure()
+    plt.suptitle("SSIM: %.4f, MSE: %.0f" % (s, m))
+    # show first image
+    ax = fig.add_subplot(1, 2, 1)
+    plt.imshow(imageA, cmap = plt.cm.gray)
+    plt.axis("off")
+    # show the second image
+    ax = fig.add_subplot(1, 2, 2)
+    plt.imshow(imageC, cmap = plt.cm.gray)
+    plt.axis("off")
+    # save figure
+    plt.savefig(os.path.join(outdir, 'tuned.jpg'))
+    plt.close()
+    
+    return
