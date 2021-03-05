f8c605d6662333690a6249bf25adb2aa81f9e1a8
Youngseo Do <ysdo364@gmail.com>
2021-03-05 00:04:29 -0800

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -4,10 +4,10 @@ import json
 import math
 from PIL import Image, ImageStat
 
-def runtime_performance_eval(lap_data, outdir):
+def runtime_performance_eval(indir, outdir):
     os.makedirs(outdir, exist_ok=True)
-    default = runtime_performance(lap_data[0])
-    tuned = runtime_performance(lap_data[1])
+    default = runtime_performance(indir[0])
+    tuned = runtime_performance(indir[1])
     similarity = np.round(1 - abs(default-tuned)/default , 3) * 100
     f = open("runtime_evaluation_result.txt", "w+")
     f.write("Runtime performance for default, non-bright settings: %s" % default)
@@ -23,23 +23,23 @@ def runtime_performance_eval(lap_data, outdir):
 
 # measures how consistent the luminescence of mobile image data is across one track lap's time, using standard deviation
 # as a metric of variability
-def runtime_performance(lap_data):
+def runtime_performance(indir):
     
     store_perceived = []
-    for i in range(len(os.listdir(lap_data))):
-        store_perceived.append(perceived_bn(lap_data, i))
+    for i in range(len(os.listdir(indir))):
+        store_perceived.append(perceived_bn(indir, i))
     
     runtime_performance = np.std(store_perceived) ** 2
     return runtime_performance
 
 # function to calculate perceived brightness which effectively measures luminescence of images using rgb color codes
-def perceived_bn(lap_data, jpg):
+def perceived_bn(indir, jpg):
     
     converted_num = "% s" % jpg
     if (jpg < 10):
-        jpg_string = lap_data + '/frame000' + converted_num + '.jpg'
+        jpg_string = indir + '/frame000' + converted_num + '.jpg'
     else:
-        jpg_string = lap_data + '/frame00' + converted_num + '.jpg'
+        jpg_string = indir + '/frame00' + converted_num + '.jpg'
     img = Image.open(jpg_string)
     stat = ImageStat.Stat(img)
     r,g,b = stat.rms
