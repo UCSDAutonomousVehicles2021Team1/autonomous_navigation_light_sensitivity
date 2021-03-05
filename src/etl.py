48fccd9d22a41be531cb88cc8307147e3dc4a768
Youngseo Do <ysdo364@gmail.com>
2021-03-04 23:40:17 -0800

    Update etl.py

--- src/etl.py
+++ src/etl.py
@@ -16,9 +16,9 @@ def load_data(single_source, lap_source, single_data_fp, lap_data_fp):
         shutil.copy(single_source + i, single_data_fp)
     
     for j in os.listdir(lap_source):
-        os.makedirs(lap_data_fp + j, exist_ok = True)
+        os.makedirs(lap_data_fp + j + '/', exist_ok = True)
         for k in os.listdir(lap_source + j):
-            shutil.copy(lap_source + j + k, lap_data_fp + j)
+            shutil.copy(lap_source + j + k, lap_data_fp + j + '/')
             
     print('After loading single image data: ' + str(os.listdir(single_data_fp)))
     print('After loading collective lap data: ' + str(os.listdir(lap_data_fp)))
