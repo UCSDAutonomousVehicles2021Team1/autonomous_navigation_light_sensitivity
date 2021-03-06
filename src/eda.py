267e6350ff0cb6c90c3f9dc8ddd029c3f47505f9
Youngseo Do <ysdo364@gmail.com>
2021-03-05 19:45:41 -0800

    Update eda.py

--- src/eda.py
+++ src/eda.py
@@ -1,9 +1,33 @@
 # import the necessary packages
-from skimage.metrics import structural_similarity as ssim
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 import pandas as pd
 
-def main_eda(files, outdir, **kwargs):
+def main_eda(datadir, outdir, **kwargs):
   
+    # access all .jpg files in the directory.
+    allfiles = os.listdir(datadir)
+    img_lst = [datadir + file for file in allfiles if file[-4:] in ['.jpg']]
+    
+    # Given that all images are the same size, get the dimensions of the first image.
+    width, height = Image.open(img_lst[0]).size
+    N = len(img_lst)
+    
+    # Create an array of floats to store the average.
+    arr = np.zeros((height, width, 3), np.float)
+    
+    # Calculate average pixel intensities, casting each image as an array of floats.
+    for i in img_lst:
+        img_arr = np.array(Image.open(i), dtype=np.float)
+        arr = arr+img_arr/N
+    
+    # Round values in an array and cast as 8-bit integer.
+    arr = np.array(np.round(arr), dtype=np.uint8)
+    
+    # Generate and save the final merged image (average pixel image of all images in a track lap data).
+    merged = Image.fromarray(arr, mode="RGB")
+    os.path.join(outdir, merged.save("average_picture.jpg"))
+    
+    return
+ 
